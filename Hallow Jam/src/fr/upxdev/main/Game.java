835b6926a41cab1b24420e811cb2acaf82393a7a
Horow <fr13031997@gmail.com>
2016-11-17 18:05:11 +0100

    Text

--- Hallow Jam/src/fr/upxdev/main/Game.java
+++ Hallow Jam/src/fr/upxdev/main/Game.java
@@ -13,35 +13,36 @@ public class Game extends Canvas implements Runnable{
 	public static int storyLine=0;
 	public static int storyColumn=0;
 	public static boolean looseHp=false;
-	
+  public static boolean test=true;
+
 	private static final long serialVersionUID = -4021390787203127274L;
-	
+
 	public static final int WIDTH = 1080, HEIGHT = WIDTH /16*9;
 	private Thread thread;
 	private boolean running = false;
-	
+
 	private Handler handler;
 	private HUD hud;
-	
+
 	public Game(){
 		handler = new Handler();
 		this.addKeyListener(new KeyInput(handler));
 		this.addMouseListener(new MouseInput(handler));
 		new Window(WIDTH, HEIGHT, "Hallow Jam", this);
-		
-		
+
+
 		handler.addObject(new Player(150,150,ID.Player));
 		handler.addObject(new Text(WIDTH-((int)(WIDTH/1.38)), 50, ID.Text));
 		hud = new HUD(handler);
-		
-		
+
+
 	}
 	public synchronized void start(){
 		thread = new Thread(this);
 		thread.start();
 		running = true;
 	}
-	
+
 	public synchronized void stop(){
 		try{
 			thread.join();
@@ -50,7 +51,7 @@ public class Game extends Canvas implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run(){
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
@@ -76,29 +77,29 @@ public class Game extends Canvas implements Runnable{
 				frames = 0;
 			}
 		}
-		
+
 		stop();
-		
+
 	}
-	
+
 	private void tick(){
 		handler.tick();
 		hud.tick();
 	}
-	
+
 	private void render(){
 		BufferStrategy bs = this.getBufferStrategy();
 		if(bs == null){
 			this.createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
-		
+
+
 		handler.render(g);
 		for(int i=0;i<handler.object.size();i++){
 			if(handler.object.get(i).id==ID.Player){
@@ -108,22 +109,22 @@ public class Game extends Canvas implements Runnable{
 					while(player.getName().length()>15){
 						player.setName(JOptionPane.showInputDialog("Quel est votre nom?"));
 					}
-					
+
 				}
 			}
-			
+
 		}
-		
+
 		hud.render(g);
-		
-		
+
+
 		g.dispose();
 		bs.show();
 	}
 	public static void main(String args[]){
 		new Game();
 	}
-	
+
 	public static int clamp(int var, int min, int max){
 		if(var>=max){
 			return var = max;
@@ -132,25 +133,25 @@ public class Game extends Canvas implements Runnable{
 		}else{
 			return var;
 		}
-		
+
 	}
-	
+
 	public static boolean checkIfPlayer(GameObject go){
 		if(go.id==ID.Player){
 			return true;
 		}else{
 			return false;
-		}	
+		}
 	}
-	
+
 	public static boolean checkIfButton(GameObject go){
 		if(go.id==ID.Button){
 			return true;
 		}else{
 			return false;
-		}	
+		}
 	}
-	
+
 	public static boolean checkIfText(GameObject go){
 		if(go.id==ID.Text){
 			return true;
@@ -158,6 +159,6 @@ public class Game extends Canvas implements Runnable{
 			return false;
 		}
 	}
-	
+
 
 }
